aea903fcd7161c9823e33ed4cdc6f8004aa672dd
Oskar Wolny <osk4r.wolny@gmail.com>
2023-05-02 18:11:56 +0200

    Create RegisterError.java

---
+++ authentication-server/src/main/java/pl/owolny/authenticationserver/controller/register/error/RegisterError.java
@@ -0,0 +1,9 @@
+package pl.owolny.authenticationserver.controller.register.error;
+
+public enum RegisterError {
+
+    PROVIDER_USER_DATA_NOT_FOUND,
+    USER_CREATION_FAILED,
+    USER_ALREADY_EXISTS,
+    TOKEN_CREATION_FAILED,
+}
